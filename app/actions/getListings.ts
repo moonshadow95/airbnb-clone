ee70c11a24836db3a3569106be10ba612e54f269
Moonshadow <last_enemy@naver.com>
2023-05-24 12:54:14 +0900

    Individual Listing View

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,15 +1,21 @@
 import prisma from "@/app/libs/prismadb"
+import {SafeListing} from "@/app/types";
 
-export default async function getListings(){
-  try{
+export default async function getListings() {
+  try {
     const listings = await prisma.listing.findMany({
-      orderBy:{
-        createdAt:'desc'
+      orderBy: {
+        createdAt: 'desc'
       }
     })
 
-    return listings
-  } catch(error:any){
+    const safeListings: SafeListing[] = listings.map(listing => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString()
+    }))
+
+    return safeListings
+  } catch (error: any) {
     throw new Error(error)
   }
 }
---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import React, {useMemo} from 'react';
+import {Reservation} from "@prisma/client";
+import {SafeListing, SafeUser} from "@/app/types";
+import {categories} from "@/app/components/navbar/Categories";
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
+
+interface ListingClientProps {
+  reservations?: Reservation[]
+  /** getListingById 에서 listing 을 반환할 때, user 를 포함시켰기 때문에 타입에 user 를 추가 */
+  listing: SafeListing & {
+    user: SafeUser
+  }
+  currentUser?: SafeUser | null
+}
+
+const ListingClient: React.FC<ListingClientProps> = (
+  {
+    listing,
+    currentUser
+  }) => {
+  const category = useMemo(() => {
+    return categories.find(item => item.label === listing.category)
+  }, [listing.category]);
+
+  return (
+    <Container>
+      <div className={`max-w-screen-lg mx-auto`}>
+        <div className={`flex flex-col gap-6`}>
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+        <div
+          className={`
+            grid
+            grid-cols-1
+            md:grid-cols-7
+            md:gap-10
+            mt-6
+         `}>
+          <ListingInfo
+            user={listing.user}
+            category={category}
+            description={listing.description}
+            roomCount={listing.roomCount}
+            guestCount={listing.guestCount}
+            bathroomCount={listing.bathroomCount}
+            locationValue={listing.locationValue}
+          />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default ListingClient;
